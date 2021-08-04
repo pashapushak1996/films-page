661578bb016135f22850b7e3214c02da2a5e8e60
pashapushak1996 <pashapushak1996@gmail.com>
2021-08-04 22:25:49 +0300

    add firebase

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, {useEffect} from "react";
 import styled from "styled-components/macro";
 import ImgSlider from "./ImgSlider";
 import Viewers from "./Viewers";
 import Movies from "./Movies";
+import db from '../firebase';
 
 const Home = () => {
+    useEffect(() => {
+        db.collection("movies").onSnapshot((snapshot) => {
+            const moviesList = snapshot.docs.map((doc) => {
+                return {id: doc.id, ...doc.data()};
+            });
+            console.log(moviesList);
+        })
+    }, [])
+
     return (
         <Container>
             <ImgSlider/>
