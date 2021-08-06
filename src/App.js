6d0facf0a00c4df7a6e298c08de924d0feb3f9b3
pashapushak1996 <pashapushak1996@gmail.com>
2021-08-06 23:08:48 +0300

    Add redirect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import Header from "./components/Header";
 import Home from "./components/Home";
-import {Route, Switch} from "react-router-dom";
+import {Redirect, Route, useHistory} from "react-router-dom";
 import MovieDetails from "./components/MovieDetails";
 import Login from "./components/Login";
+import {useSelector} from "react-redux";
+import {selectUser} from "./features/user/userSlice";
 
 function App() {
+
+    const user = useSelector(selectUser);
 
     return (
         <div>
             <Header/>
-            <Switch>
-                <Route exact path={ `/` } component={ Home }/>
-                <Route path={ `/movie/:id` } component={ MovieDetails }/>
-                <Route path={ `/login` } component={ Login }/>
-            </Switch>
+            { !user.name ? <Redirect to={ `/login` }/> : <Redirect to={ `/` }/> }
+            <Route exact path={ `/` } component={ Home }/>
+            <Route path={ `/movie/:id` } component={ MovieDetails }/>
+            <Route path={ `/login` } component={ Login }/>
         </div>
     );
 }
